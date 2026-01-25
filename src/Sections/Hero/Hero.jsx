ca66066bcb72f41bb09ec86910269b8f657e161f
Jatin29yadav <jy481658@gmail.com>
2026-01-25 20:49:05 +0530

    Minor update in Hero section

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -1,10 +1,8 @@
-import ShortText from "../../Components/Hero/ShortText";
 import HeroContent from "./HeroContent";
 
 const Hero = () => {
   return (
-    <div className="h-screen w-full text-white flex flex-col justify-center gap-10 items-center">
-      <ShortText />
+    <div className="h-screen w-full text-white flex justify-center items-center">
       <HeroContent />
     </div>
   );
--- src/Sections/Hero/HeroContent.jsx
+++ src/Sections/Hero/HeroContent.jsx
@@ -1,20 +1,49 @@
 import Button from "../../Components/Buttons/Button";
+import ShortText from "../../Components/Hero/ShortText";
+import { gsap } from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
 
 const HeroContent = () => {
+  const heroRef = useRef(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+
+      tl.from(heroRef.current.children, {
+        scale: 1.3,
+        y: 50,
+        opacity: 0,
+        duration: 1.5,
+        stagger: 0.3,
+        ease: "back.out(1.7)",
+      });
+    },
+    { scope: heroRef },
+  );
+
   return (
-    <>
-      <div className="flex flex-col gap-10 text-center items-center">
-        <div className="font-medium lg:text-[4.5rem] sm:text-[3.5rem] text-[2rem] font2 leading-tight">
-          <div>Spiral Into The,</div>
-          <div>Endless Unknown.</div>
-        </div>
-        <div className="sm:w-7/12 w-10/12 md:text-[1.5rem]">
-          Where gravity bends reality and light paints the darkness. Navigate a
-          user experience built on the laws of physics and imagination.
-        </div>
+    <div
+      ref={heroRef}
+      className="flex flex-col gap-10 text-center items-center justify-center h-full relative pointer-events-none"
+    >
+      <ShortText />
+
+      <div className="font-medium lg:text-[4.5rem] sm:text-[3.5rem] text-[2rem] font2 leading-tight">
+        <div>Spiral Into The,</div>
+        <div>Endless Unknown.</div>
+      </div>
+
+      <div className="sm:w-7/12 w-10/12 md:text-[1.5rem] text-gray-300">
+        Where gravity bends reality and light paints the darkness. Navigate a
+        user experience built on the laws of physics and imagination.
+      </div>
+
+      <div className="pointer-events-auto">
         <Button />
       </div>
-    </>
+    </div>
   );
 };
 
